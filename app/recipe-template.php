5f5a046a5162107dfee68d18adfe68418a533345
Anaïs <anais75.b@gmail.com>
2024-05-25 17:24:56 +0200

    Starting recipe template

---
+++ app/recipe-template.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once(__DIR__ . '/core/init.php');
+require_once(__DIR__ . '/config/mysql.php');
+require_once(__DIR__ . '/databaseconnect.php');
+$pageTitle = 'Recette';
+
+ob_start();
+
+$id = $_GET['id'];
+$sql = 'SELECT * from recipes WHERE id = :id';
+$stmt = $mysqlClient->prepare($sql);
+$stmt->execute(['id' => $id]);
+$recipe = $stmt->fetch();
+?>
+
+<section id="recipeTemplate-section">
+    <div class="container p-5">
+        <div class="mx-auto mb-5 p-5 border shadow-sm bg-white rounded-block">
+            <?php if($recipe): ?>
+                <h1 class="text-center mb-5"><?php echo htmlspecialchars($recipe['title']) ?></h1>
+                <div>
+                    <h2>Ingrédients</h2>
+                    <p><?php echo htmlspecialchars($recipe['ingredients'])?></p>
+                </div>
+                <div>
+                    <h2>Ustensiles</h2>
+                    <p><?php echo htmlspecialchars($recipe['tools'])?></p>
+                </div>
+                <div>
+                    <h2>Préparation</h2>
+                    <p><?php echo htmlspecialchars($recipe['description'])?></p>
+                </div>
+            <?php else: ?>
+                <p>La recette n'existe pas.</p>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
+
+
+<?php
+$content = ob_get_clean();
+require_once(__DIR__ . '/views/layout.php');
+?>
--- app/views/widget.php
+++ app/views/widget.php
@@ -1,3 +1,15 @@
+<?php
+require_once(__DIR__ . '/../core/init.php');
+require_once(__DIR__ . '/../config/mysql.php');
+require_once(__DIR__ . '/../databaseconnect.php');
+
+$sql = "SELECT * from recipes";
+$stmt = $mysqlClient->query($sql);
+
+$recipes = $stmt->fetchAll(PDO::FETCH_ASSOC)
+
+?>
+
 <!-- widget.php -->
 <div class="container my-5" id="last-recipes">
     <div class="row mb-3">
@@ -12,6 +24,7 @@
                 <h3><?php echo $recipe['title']; ?></h3>
                 <h3><?php echo $recipe['user_id']; ?></h3>
                 <div><?php echo $recipe['rating']; ?></div>
+                <button type="button" class="btn btn-secondary"><a href="recipe-template.php?id=<?php echo htmlspecialchars($recipe['id'])?>">Voir</a></button>
             </div>
         <?php endforeach ?>
     </div>
